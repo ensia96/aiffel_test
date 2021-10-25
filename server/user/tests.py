2c123aaabd52ba856731e98b9cdee052b9f40ab2
춤추는망고 <ensia96@gmail.com>
2021-10-25 21:59:44 +0900

    [add] set up function and test case with duplicated user data

--- server/user/tests.py
+++ server/user/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.db import transaction
 from .models import User
 
 CONTENT_TYPE = "application/json"
@@ -7,12 +8,23 @@ CONTENT_TYPE = "application/json"
 
 class SignUpTest(TestCase):
     URL = reverse("sign-up")
+    PAYLOAD = {"content_type": CONTENT_TYPE}
+
+    def setUp(self):
+        User.objects.create(
+            username="test_id_1",
+            password="test_password",
+            nickname="test_user",
+            email="email@for.test_1",
+            age=20,
+            phone="010-0000-0000",
+        )
 
     def tearDown(self):
         User.objects.all().delete()
 
     def test_signup_success(self):
-        data = {
+        self.PAYLOAD["data"] = {
             "username": "test_id",
             "password": "test_password",
             "nickname": "test_user",
@@ -21,12 +33,12 @@ class SignUpTest(TestCase):
             "phone": "010-0000-0000",
         }
 
-        res = self.client.post(self.URL, data=data, content_type=CONTENT_TYPE)
+        res = self.client.post(self.URL, **self.PAYLOAD)
         self.assertEqual(res.status_code, 201)
 
     def test_signup_without_certain_key(self):
         keys = ["username", "password", "nickname", "email", "age", "phone"]
-        data = {
+        self.PAYLOAD["data"] = {
             "username": "test_id",
             "password": "test_password",
             "nickname": "test_user",
@@ -36,10 +48,35 @@ class SignUpTest(TestCase):
         }
 
         for key in keys:
-            value = data.pop(key)
+            value = self.PAYLOAD["data"].pop(key)
 
-            res = self.client.post(self.URL, data=data, content_type=CONTENT_TYPE)
+            res = self.client.post(self.URL, **self.PAYLOAD)
             self.assertEqual(res.json().get("message"), f"'{key}' is not provided.")
             self.assertEqual(res.status_code, 400)
 
-            data[key] = value
+            self.PAYLOAD["data"][key] = value
+
+    def test_signup_with_duplicated_user_data(self):
+        keys = ["username", "email"]
+        self.PAYLOAD["data"] = {
+            "username": "test_id",
+            "password": "test_password",
+            "nickname": "test_user",
+            "email": "email@for.test",
+            "age": 20,
+            "phone": "010-0000-0000",
+        }
+
+        for key in keys:
+            self.PAYLOAD["data"][key] += "_1"
+
+            try:
+                with transaction.atomic():
+                    res = self.client.post(self.URL, **self.PAYLOAD)
+
+                self.assertEqual(res.json().get("message"), f"{key} aleady exists.")
+                self.assertEqual(res.status_code, 400)
+
+                self.PAYLOAD["data"][key] = self.PAYLOAD["data"][key][:-2]
+            except Exception:
+                pass
