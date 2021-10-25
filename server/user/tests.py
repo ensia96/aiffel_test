a89eaf5770620f813cb5b491eac774083339feff
춤추는망고 <ensia96@gmail.com>
2021-10-25 21:27:19 +0900

    [add] signup test case - without certain key

--- server/user/tests.py
+++ server/user/tests.py
@@ -23,3 +23,23 @@ class SignUpTest(TestCase):
 
         res = self.client.post(self.URL, data=data, content_type=CONTENT_TYPE)
         self.assertEqual(res.status_code, 201)
+
+    def test_signup_without_certain_key(self):
+        keys = ["username", "password", "nickname", "email", "age", "phone"]
+        data = {
+            "username": "test_id",
+            "password": "test_password",
+            "nickname": "test_user",
+            "email": "email@for.test",
+            "age": 20,
+            "phone": "010-0000-0000",
+        }
+
+        for key in keys:
+            value = data.pop(key)
+
+            res = self.client.post(self.URL, data=data, content_type=CONTENT_TYPE)
+            self.assertEqual(res.json().get("message"), f"'{key}' is not provided.")
+            self.assertEqual(res.status_code, 400)
+
+            data[key] = value
