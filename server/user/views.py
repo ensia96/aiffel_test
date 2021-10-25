0c05869622cd60366510522f468ba90097a8dbfc
춤추는망고 <ensia96@gmail.com>
2021-10-25 18:11:39 +0900

    [create] signup view function

--- server/user/views.py
+++ server/user/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.contrib.auth.hashers import make_password
+from django.db.utils import IntegrityError
+from django.http import JsonResponse as res
+
+from .models import User
+
+
+def signup(req):
+    if req.method != "POST":
+        return res({"message": "this method is not allowed."}, status=400)
+
+    data = json.loads(req.body)
+
+    try:
+        User.objects.create(
+            username=data["username"],
+            password=make_password(data["password"]),
+            nickname=data["nickname"],
+            email=data["email"],
+            age=data["age"],
+            phone=data["phone"],
+        )
+
+    except KeyError as E:
+        return res({"message": str(E) + " is not provided."}, status=400)
+
+    except IntegrityError as E:
+        return res({"message": str(E).split(".").pop() + " aleady exists."}, status=400)
+
+    return res({"message": "user data is "}, status=201)
