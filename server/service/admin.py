7924f3b8c67f6cefa3e3076325a957534b70523f
춤추는망고 <ensia96@gmail.com>
2021-10-26 19:49:38 +0900

    [add] post model to admin page

--- server/service/admin.py
+++ server/service/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "like", "created_at")
+    list_display_links = ("title",)
+
+    def author(self, obj):
+        return obj.user.nickname
+
+    def like(self, obj):
+        return len(obj.likeforpost_set.all())
+
+    author.admin_order_field = 'user'
+    author.short_description = '작성자'
+    like.short_description = '좋아요'
