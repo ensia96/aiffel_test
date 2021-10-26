6ccb88371eb90042c7413a1fbdb706334483d6b9
춤추는망고 <ensia96@gmail.com>
2021-10-26 19:50:51 +0900

    [add] comment model to admin page

--- server/service/admin.py
+++ server/service/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -12,6 +12,22 @@ class PostAdmin(admin.ModelAdmin):
 
     def like(self, obj):
         return len(obj.likeforpost_set.all())
+
+    author.admin_order_field = 'user'
+    author.short_description = '작성자'
+    like.short_description = '좋아요'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("content", "author", "like", "created_at")
+    list_display_links = ("content",)
+
+    def author(self, obj):
+        return obj.user.nickname
+
+    def like(self, obj):
+        return len(obj.likeforcomment_set.all())
 
     author.admin_order_field = 'user'
     author.short_description = '작성자'
