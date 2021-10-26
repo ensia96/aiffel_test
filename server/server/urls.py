532a72a490ae4526efffd06fa4cc4a2882c0c024
춤추는망고 <ensia96@gmail.com>
2021-10-26 19:47:40 +0900

    [add] service url pattern to main url pattern

--- server/server/urls.py
+++ server/server/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("user.urls"), name="user"),
+    path("service/", include("service.urls"), name="service")
 ]
