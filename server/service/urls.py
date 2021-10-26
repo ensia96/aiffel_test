ae2c5faee0e50d0c4d8dafb47f8be9c73fc6e57d
춤추는망고 <ensia96@gmail.com>
2021-10-26 21:18:00 +0900

    [add] url pattern for get_post function

--- server/service/urls.py
+++ server/service/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("post/create/", views.create_post, name="create-post"),
     path("posts/", views.get_post_list, name="get-post-list"),
+    path("post/<int:post_id>/", views.get_post, name="get-post"),
 ]
