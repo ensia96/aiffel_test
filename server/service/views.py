310969f21ea179c8c4bf58817adde1e6bf2d40fd
춤추는망고 <ensia96@gmail.com>
2021-10-26 19:46:30 +0900

    [create] create_post view function

--- server/service/views.py
+++ server/service/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse as res
+
+from .models import Post
+from user.models import User
+
+
+def create_post(req):
+    if req.method != "POST":
+        return res({"message": "this method is not allowed."}, status=400)
+
+    data = json.loads(req.body)
+
+    try:
+        Post.objects.create(
+            title=data['title'],
+            content=data['content'],
+            user=User.objects.get(id=1)
+        )
+
+    except Exception as E:
+        return res({"message": str(E) + " is not provided."}, status=400)
+
+    return res({"message": "post creation success"}, status=201)
