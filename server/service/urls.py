300e8ddb8ee58dfa094f37944f6914eafbc54307
춤추는망고 <ensia96@gmail.com>
2021-10-27 19:32:49 +0900

    [add] url pattern for get_comment_list function

--- server/service/urls.py
+++ server/service/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("post/delete/<int:post_id>/", views.delete_post, name="delete-post"),
     path("post/like/<int:post_id>/", views.like_post, name="like-post"),
     path("comment/add/", views.add_comment, name="add-comment"),
+    path("comments/<int:post_id>", views.get_comment_list, name="get-comment-list"),
 ]
