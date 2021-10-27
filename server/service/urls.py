0578f7b50960e52652f3cf8dce12bf44a4801e42
춤추는망고 <ensia96@gmail.com>
2021-10-27 20:46:49 +0900

    [add] url pattern for get_top_post function

--- server/service/urls.py
+++ server/service/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("post/create/", views.create_post, name="create-post"),
     path("posts/", views.get_post_list, name="get-post-list"),
     path("post/search/", views.search_post, name="search-post"),
+    path("post/top/", views.get_top_post, name="get-top-post"),
     path("post/<int:post_id>", views.get_post, name="get-post"),
     path("post/update/", views.update_post, name="update-post"),
     path("post/delete/<int:post_id>", views.delete_post, name="delete-post"),
