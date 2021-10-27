f7d47ca23e7fb2f100bc2e4ad386e8c4344e1d2a
춤추는망고 <ensia96@gmail.com>
2021-10-27 14:39:26 +0900

    [add] url pattern for add_comment function

--- server/service/urls.py
+++ server/service/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("post/update/", views.update_post, name="update-post"),
     path("post/delete/<int:post_id>/", views.delete_post, name="delete-post"),
     path("post/like/<int:post_id>/", views.like_post, name="like-post"),
+    path("comment/add/", views.add_comment, name="add-comment"),
 ]
